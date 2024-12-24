3264a42cfe31c2663ae1c1d01d371557a1270de7
gseebach <117525470+gseebach@users.noreply.github.com>
2024-12-24 12:32:51 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+
+// const santa=document.getElementById("schlitten");
+
+//$("#schlitten").animate({transform: "200px"},3000);
+//document.querySelector(".titel").classList.add("farbe");
+$(document).ready(function(){
+  $("#schlitten").animate({right: "2000px", top: "20vh", width: "300px", height: "200px"},{duration: 5000, iterations: 1});
+});
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+ 
+
